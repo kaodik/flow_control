cf57d8ee8e0ba081401e4fdb00b1b27e054a7b9c
kao <trumblays@yahoo.com>
2023-01-23 11:25:45 -0600

    added while and for loop

--- src/main.rs
+++ src/main.rs
@@ -43,4 +43,63 @@ fn main() {
     // loop {
     //     println!("again!")
     // }
+    // can apply the break to leave the loop
+    
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+
+    //nested loops applying loop labels with single qoute '
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    //while loop
+    // the following way to loop through is error prone
+    //if it does reach end and call for index that is out of bounds
+    // it will panic.
+    // it also adds the runtime code to check if the index is within bounds
+    let mut num2 = 3;
+    while num2 != 0 {
+        println!("{num2}!");
+        num2 -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    //loop through array
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index += 1;
+    }
+
+    //use a for loop instead
+    for element in a {
+        println!("the value is: for {element}");
+    }
 }
